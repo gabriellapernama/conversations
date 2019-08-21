bc99abd1ceb05aec19e95d0f61a7b801ff9be3ce
Gabriella Pernama <gabypernama@gmail.com>
2019-08-21 15:36:57 -0700

    added to index.js for trial and error purposes

--- main-app/src/pages/index.js
+++ main-app/src/pages/index.js
@@ -1,3 +1,13 @@
-import React from "react"
+import React from "react";
+import Header from '../components/header.js'; 
+import {Link} from 'gatsby';
 
-export default () => <div>Hello world!</div>
+export default () => {
+return <div style={{color: `purple`, fontSize: `24px`}}>
+    <Header headerText="Home"/>
+    <Link to="/contact/">Contact us today!</Link> 
+    <br/>
+    <Link to="/about/">About Me</Link>
+    <br/><img src="https://source.unsplash.com/random/400x200" alt=""/>
+</div>
+}
